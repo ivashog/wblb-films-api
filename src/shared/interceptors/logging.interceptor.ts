53b6454163a676f8b050360a023e964bd93efb8f
Olexiy Ivashkin <ivashkinog@gmail.com>
2020-09-09 12:08:18 +0300

    refactor(logging): refactor simple stdout logger interceptor

--- src/shared/interceptors/logging.interceptor.ts
+++ src/shared/interceptors/logging.interceptor.ts
@@ -1,19 +1,60 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import {
+    CallHandler,
+    ExecutionContext,
+    HttpException,
+    Injectable,
+    Logger,
+    NestInterceptor,
+} from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { Request } from 'express';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-        const request = context.switchToHttp().getRequest();
-        const method = request.method;
-        const url = request.url;
         const now = Date.now();
 
         return next.handle().pipe(
             tap(() => {
-                Logger.log(`${method} ${url} ${Date.now() - now}ms `, context.getClass().name);
+                this.logSuccess(context, now);
+            }),
+            catchError(err => {
+                this.logError(context, err);
+                return throwError(err);
             }),
         );
     }
+
+    protected logSuccess = (context: ExecutionContext, reqTime: number): string => {
+        const caller = context.getClass().name;
+        const { method, url } = context.switchToHttp().getRequest<Request>();
+        const message = `${method} ${url} per ${Date.now() - reqTime} ms`;
+
+        Logger.log(message, caller);
+
+        return message;
+    };
+
+    protected logError = (
+        context: ExecutionContext,
+        err: Error | HttpException,
+    ): { error: string; errorDetail?: string } => {
+        const caller = context.getClass().name;
+        const { method, url } = context.switchToHttp().getRequest<Request>();
+        const error = this._isHttpException(err)
+            ? (err as any).getResponse()?.error || ''
+            : err.message;
+
+        const errorDetail = this._isHttpException(err)
+            ? (err as any).getResponse()?.message
+            : undefined;
+
+        Logger.error(`${method} ${url} Error: ${error}`, errorDetail ?? error.stack, caller, false);
+
+        return { error, errorDetail };
+    };
+
+    private _isHttpException = (error: any): error is HttpException =>
+        error instanceof HttpException;
 }
